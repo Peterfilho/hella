e6405e3e17fa84092c883348c08ea94a2f0f7ec0
Peterson <peterson.petio@gmail.com>
2020-11-04 14:16:18 -0300

    Project created and Add some templates

---
+++ hella/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contato', views.contact, name='contact'),
+    path('produto', views.product, name='product'),
+    path('produtos', views.product_list, name='product_list'),
+    path('admin/', admin.site.urls),
+]
