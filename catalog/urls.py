4baf99fa84e5c7652f983d60aba6bd557b39b372
Peterson <peterson.petio@gmail.com>
2020-11-07 18:13:15 -0300

    Add catalog and product on views

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,5 @@ app_name='catalog'
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     re_path(r'^(?P<slug>[\w-]+)/$', views.category, name='category'),
+    re_path(r'^produtos/(?P<slug>[\w-]+)/$', views.product, name='product'),
 ]
