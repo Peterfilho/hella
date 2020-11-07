05fa51d60212b677bdb69375230720c77e677279
Peterson <peterson.petio@gmail.com>
2020-11-07 12:16:40 -0300

    Add product list views and templates on products/

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
@@ -10,6 +9,3 @@ def contact(request):
 
 def product(request):
     return render(request, 'product.html')
-
-def product_list(request):
-    return render(request, 'product_list.html')
--- hella/urls.py
+++ hella/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('contato', views.contact, name='contact'),
     path('produto', views.product, name='product'),
-    path('produtos', views.product_list, name='product_list'),
+    path('produtos/', include(('catalog.urls'), namespace='catalog')),
     path('admin/', admin.site.urls),
 ]
