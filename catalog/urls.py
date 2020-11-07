596b25e2751fed7007a0f96b8323a3c0c4d275fa
Peterson <peterson.petio@gmail.com>
2020-11-07 14:25:01 -0300

    Add catalog on views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name='catalog'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.category, name='category'),
 ]
